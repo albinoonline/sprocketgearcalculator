41e66ef721bca1ed47d125fbb7fc7afd44e82fa2
albino <46507585+albinoonline@users.noreply.github.com>
2021-12-31 10:08:41 -0800

    Update editor.js

--- editor.js
+++ editor.js
@@ -9,41 +9,18 @@ window.onload = function() {
 	//get output
 	const output = document.getElementById('output');
 	
-	//take the ratio of the previous gear, multiply by target min, divided by target max
-	//G2 = G1 * min/max
-	//G2 = G1 * min * 1/max
-	//G2/min = G1 * 1/max
-	//G2/min = G1/max
-	//min/G2 = max/G1
-	//min = G2* max/G1
-	/*
-	works for 2 gears: min = G2* max/G1
-	min = G2* max/G1
-	
-	each step is a fixed multiple of the last,
-	so gear(x)= first*(b)^2, we have the point (gears,top)
-	b is min/max
-	once we solve for b:
-	each whole value of x is a valid gear.
-	to solve:
-	
-	top = first(b)^gears
-	top/first = b^gears
-	(top/first)^(1/gears) = b
-	
-	*/
 	go.addEventListener("click", function(){
 		//get b, (top/first)^(1/gears) = b
 		let b = Math.pow(top.value/first.value,1/(gears.value-1));
 		//get min RPM
-		output.innerHTML=`<p>Minimum RPM:${Math.round(max.value*b)}</p>`;
+		output.innerHTML=`<p>Minimum RPM: ${Math.round(max.value*b)}</p>`;
 		//make a functiuon to call gears
 		function gear(x){
 			//first*(b)^2
 			return Math.round(first.value*Math.pow(b,x) * 100) / 100;
 		}
 		for(let i = 0; i < gears.value; i++){
-			output.innerHTML+=`<p>Gear ${i+1}:${gear(i)}</p>`;
+			output.innerHTML+=`<p>Gear ${i+1}: ${gear(i)}</p>`;
 		}
 	});
 }
