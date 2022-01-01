65739e1f64f2eefabacb9aab5d3a9576d63d13bf
albino <46507585+albinoonline@users.noreply.github.com>
2021-12-31 22:45:53 -0800

    minor formatting changes

--- editor.js
+++ editor.js
@@ -9,6 +9,29 @@ window.onload = function() {
 	//get output
 	const output = document.getElementById('output');
 	
+	//take the ratio of the previous gear, multiply by target min, divided by target max
+	//G2 = G1 * min/max
+	//G2 = G1 * min * 1/max
+	//G2/min = G1 * 1/max
+	//G2/min = G1/max
+	//min/G2 = max/G1
+	//min = G2* max/G1
+	/*
+	works for 2 gears: min = G2* max/G1
+	min = G2* max/G1
+	
+	each step is a fixed multiple of the last,
+	so gear(x)= first*(b)^2, we have the point (gears,top)
+	b is min/max
+	once we solve for b:
+	each whole value of x is a valid gear.
+	to solve:
+	
+	top = first(b)^gears
+	top/first = b^gears
+	(top/first)^(1/gears) = b
+	
+	*/
 	go.addEventListener("click", function(){
 		//get b, (top/first)^(1/gears) = b
 		let b = Math.pow(top.value/first.value,1/(gears.value-1));
